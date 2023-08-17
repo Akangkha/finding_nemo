7fc9fd56e1dbddbdb467098b7fe3addd73384d74
kpriyanshu2003 <az.priyanshu.rockstar@gmail.com>
2023-08-17 22:12:32 +0530

    Updated Navbar

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,27 +1,20 @@
 import React from "react";
 import "../styles/Navbar.css";
 import { Link } from "react-router-dom";
-import BookmarkAddedIcon from "@mui/icons-material/BookmarkAdded";
-import { Tooltip } from "@mui/material";
 
 const NavBar = (props) => {
   return (
     <div className="navBar">
-      <Link to="/" className="cursor-pointer title ">
+      <Link to="/" className="cursor-pointer title">
         Finding Nemo
       </Link>
       <div>
         <div
-          className="color-blue cursor-pointer navItem navButton"
+          className=" cursor-pointer navItem navButton"
           onClick={() => props.updateItemLost()}
         >
           Report an item !
         </div>
-        <Tooltip title="Claimed items" arrow>
-          <Link to="/claimed" style={{ textDecoration: "none" }}>
-            <BookmarkAddedIcon className="color-blue cursor-pointer navItem" />
-          </Link>
-        </Tooltip>
       </div>
     </div>
   );
